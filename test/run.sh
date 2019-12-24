36ec92fcda5257e2cbbe76d721fb070722340508
bakerwm <wangmcas@gmail.com>
2019-12-24 17:32:14 +0800

    update test

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,20 @@
 # hiseq  qc -i data/pe_rep1_1.fq.gz -o aaa -m 20
 # hiseq qc -i data/pe_rep1_1.fq.gz -o ccc -m 20 --rmdup 
 # hiseq qc -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o aaa --library-type 1
-hiseq qc -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o aaa -m 20 -a AGATCGGAAGAGCACA --threads 4 --rmdup --cut-after-trim 9,-6
+# hiseq qc -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o aaa -m 20 -a AGATCGGAAGAGCACA --threads 4 --rmdup --cut-after-trim 9,-6
+
+ 
+# hiseq align -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o aaa -g dm3 --aligner bowtie -n demo --unique-only --align-to-rRNA --threads 2
+
+# fq1="data/pe_rep1_1.fq.gz"
+# fq2="data/pe_rep1_2.fq.gz"
+# outdir="results/align"
+# hiseq align -i $fq1 --fq2 $fq2 -o $outdir -g dm3 -k mm10 --aligner bowtie2 -n demo --unique-only --align-to-rRNA --threads 2 --extra-para "-X 2000"
+
+# hipipe-align.py -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o bbb -g dm3 --aligner bowtie2 -n demo --unique-only --align-to-rRNA 
+
+
+
+# hiseq align -i data/pe_rep1_1.fq.gz --fq2 data/pe_rep1_2.fq.gz -o results/align -g dm6 --aligner bowtie2 -n demo --extra-para "-X 2000" --n-map 0 --unique-only --align-to-chrM 
+
+
--- test/test.py
+++ test/test.py
@@ -3,13 +3,15 @@
 ## test
 
 import hiseq
-from alignment import *
-from helper import *
+from hiseq.utils.helper import *
+from hiseq.utils import *
+from call_peak import *
+from rep_cor import *
 
-## qc
-fq1 = 'data/pe_rep1_1.fq.gz'
-fq2 = 'data/pe_rep1_2.fq.gz'
-outdir = 'results/qc'
+# ## qc
+# fq1 = 'data/pe_rep1_1.fq.gz'
+# fq2 = 'data/pe_rep1_2.fq.gz'
+# outdir = 'results/qc'
 
 ## cutadapt
 # hiseq.qc.trimmer.Cutadapt(fq1, outdir).run()
@@ -17,7 +19,7 @@ outdir = 'results/qc'
 
 ## trimmer
 # hiseq.qc.trimmer.Trimmer(fq1, outdir).run()
-hiseq.qc.trimmer.Trimmer(fq1, outdir, fq2, cut_after_trim='9,-6').run()
+# hiseq.qc.trimmer.Trimmer(fq1, outdir, fq2, cut_after_trim='9,-6').run()
 
 # outdir = 'results/align'
 # 
@@ -40,3 +42,52 @@ hiseq.qc.trimmer.Trimmer(fq1, outdir, fq2, cut_after_trim='9,-6').run()
 # ## Json
 # f = 'pe_rep1.json'
 # # print(Json(f).dict)
+
+
+
+## Peak
+bam = 'results/align/demo/2.genome/demo.bam'
+genome = "dm6"
+output = "results/peak"
+prefix = "demo"
+atac = True
+
+# Macs2(bam, genome, output, prefix, atac=True).callpeak()
+# m = Macs2(bam, genome, output, prefix, atac=True)
+
+# p = m.annotation()
+# print(p)
+
+# Bam(bam).index()
+
+# peak="results/peak/demo_peaks.narrowPeak"
+# f = cal_FRiP(peak, bam)
+# print(f)
+
+
+# bam = 'ATACseq_DaGal4Xsh3893_3h_rep1.bam'
+# b1 = 'aaa.rmdup.bam'
+# b2 = 'aaa.proper_pair.bam'
+# b1 = Bam(bam).rmdup('aaa.rmdup.bam')
+# b2 = Bam(b1).proper_pair('aaa.proper_pair.bam')
+
+# # call peaks
+# Macs2(b2, "dm6", "results/peak", "aaa", atac=True).callpeak()
+
+# peak = "results/peak/aaa_peaks.narrowPeak"
+# f = cal_FRiP(peak, b2)
+# print(f)
+
+# len_dist
+# x = frag_length(b2, "aaa.len_dist.txt")
+# # print(x)
+
+# bed_list = ['x.bed', 'y.bed']
+# tiffout = 'aaa.tiff'
+# x = peak_overlap2(bed_list, tiffout)
+# print(x)
+
+# bed_list = ['demo_rep1_peaks.narrowPeak', 'demo_rep2_peaks.narrowPeak']
+# outdir = 'qc'
+# # peak_idr(bed_list, outdir)
+# peak_overlap2(bed_list, outdir)
