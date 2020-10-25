4a094d966e2748ece31de6f5ea8ea91d3e4e46de
bakerwm <wangmcas@gmail.com>
2020-10-25 20:26:30 +0800

    fix typo

--- hiseq/chipseq/chipseq.py
+++ hiseq/chipseq/chipseq.py
@@ -117,24 +117,24 @@ def init_cpu(threads=1, parallel_jobs=1):
 
 
 def update_obj(obj, d, force=True, remove=False):
-        """
-        d: dict
-        force: bool, update exists attributes
-        remove: bool, remove exists attributes
-        Update attributes from dict
-        force exists attr
-        """
-        # fresh start
-        if remove is True:
-            for k in obj.__dict__:
-                delattr(obj, k)
-        # add attributes
-        if isinstance(d, dict):
-            for k, v in d.items():
-                if not hasattr(obj, k) or force:
-                    setattr(obj, k, v)
+    """
+    d: dict
+    force: bool, update exists attributes
+    remove: bool, remove exists attributes
+    Update attributes from dict
+    force exists attr
+    """
+    # fresh start
+    if remove is True:
+        for k in obj.__dict__:
+            delattr(obj, k)
+    # add attributes
+    if isinstance(d, dict):
+        for k, v in d.items():
+            if not hasattr(obj, k) or force:
+                setattr(obj, k, v)
 
-        return obj
+    return obj
 
 
 class ChIPseqR1Config(object):
